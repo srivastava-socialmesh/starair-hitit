33d83d8e83e9106d3467ee29123ed066663a33ba
Srivastava <srivastava.g@live.com>
2026-06-28 10:26:36 +0530

    Downgrade to Tailwind CSS v3

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -4,9 +4,8 @@ import autoprefixer from 'autoprefixer';
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
-
 export default config;
